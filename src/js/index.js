2d9e9690421d4a02beefe12ece3d449adbbf4a2c
Dima <ctaricc@mail.ru>
2023-10-28 14:34:45 +0300

    feat: guarantees section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
+import { Fancybox } from "@fancyapps/ui";
 import './components';
+
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import '../scss/styles.scss';
+
+Fancybox.bind('[data-fancybox]', {
+    Images: {
+        zoom: false,
+    },
+});
+
