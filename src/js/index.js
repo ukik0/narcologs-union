8b7d3cc6403dd70c1b941c06a1fb34adfd89c932
Dima <ctaricc@mail.ru>
2023-11-06 00:27:11 +0300

    feat: burger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,40 @@
-import { Fancybox } from "@fancyapps/ui";
+import $ from 'jquery';
+
+import { Fancybox } from '@fancyapps/ui';
 import './components';
 
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 import '../scss/styles.scss';
 
 Fancybox.bind('[data-fancybox]', {
     Images: {
-        zoom: false,
-    },
+        zoom: false
+    }
 });
 
+const burgerMenuButton = document.querySelector('.header__burger');
+const closeBurgerButton = document.querySelector('.header__burger-menu-heading .close');
+
+burgerMenuButton.addEventListener('click', () => {
+    $('.header__burger-menu').addClass('--active');
+    $('body').addClass('locked')
+});
+
+closeBurgerButton.addEventListener('click', () => {
+    $('.header__burger-menu').removeClass('--active');
+    $('body').removeClass('locked')
+});
+
+const burgerMenuDropdown = document.querySelectorAll('.header__burger-menu-item');
+
+burgerMenuDropdown.forEach((dropdown) => {
+    dropdown.addEventListener('click', () => {
+        const arrow = dropdown.querySelector('.header__burger-menu-item-title');
+        const list = dropdown.querySelector('.header__burger-menu-dropdown-list');
+
+        if (!list) return;
+
+        $(list).slideToggle().css('display', 'flex');
+        $(arrow).toggleClass('--active');
+    });
+});
